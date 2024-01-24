f386d19df48c2b1b74b5b0dfcf95c49c863376bb
Kevin Wamae <wamaekevin@gmail.com>
2024-01-24 14:15:17 -0500

    update analysis script

--- nanopore/scripts/4_seekdeep_analysis.sh
+++ nanopore/scripts/4_seekdeep_analysis.sh
@@ -1,136 +1,125 @@
 #!/bin/bash
 #*********************************************************
-# VERSION 2
+# This script is for running SeekDeep analysis on nanopore data_dir.
+# It should be run from the root of the project directory
 
-# ********************************************************
+# ========================================================
 #--- Start of slurm commands ---
-# ********************************************************
+# ========================================================
 
-#SBATCH -J CRT
+#SBATCH -J SeekDeep
 #SBATCH --time=01:00:00
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=10
-#SBATCH --mem=25G
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=10G
 #SBATCH -o job.%j.out
 #SBATCH -e job.%j.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=wamaekevin@gmail.com
 
-# ********************************************************
-# load modules
-# ********************************************************
+# ========================================================
+# load modules, if needed, otherwise load conda environment with these tools
+# ========================================================
 
-# module load gcc/10.2
-# module load samtools/1.16.1
-# module load bowtie2/2.4.2
+# module load gcc
+# module load samtools
+# module load bowtie2
 
-# ********************************************************
-# activate conda environment
-# ********************************************************
+# ========================================================
+# activate conda environment, if not using modules
+# ========================================================
 
 source "${HOME}/mambaforge/etc/profile.d/conda.sh"
 conda activate seekdeep
 
-# ********************************************************
-# working directories and files
-# ********************************************************
+# ========================================================
+# variables for SeekDeep analysis
+# ========================================================
 
-# input directories and files
+# directories and files
 # --------------------------------------------------------
-dirWrk=/nfs/jbailey5/baileyweb/colabs/kwamae/projects/ade_ont_tads_nigeria_seekdeep
-dirData=$dirWrk/input/fastq_barcodes_merged
+wd=${PWD}
+data_dir=$wd/input/fastq_barcodes_merged
 
 # sample names
 # --------------------------------------------------------
-sampleNames_2x150=$dirWrk/input/run_files/sampleNames_2x150.txt
+sampleNames=$wd/input/run_files/sampleNames.txt
 
-# resources (genome, gff, known mutations)
-resources=/nfs/jbailey5/baileyweb/colabs/kwamae/resources/seekdeep_ref_genomes/plasmodium_plasmodb_pacbio
+# reference (genome, gff, known mutations)
+# --------------------------------------------------------
+reference=$wd/input/genomes
 
 # primers
 # --------------------------------------------------------
-primers_2x150=$dirWrk/input/run_files/primers_2x150.txt
+primers=$wd/input/run_files/primers.txt
 
-# output directories per primer set
+# output directory, rename to user's preference
 # --------------------------------------------------------
-dirOut_2x150=$dirWrk/output/2023_12_03-CRT
+out_dir=$wd/output/2024_01_01-test
 
-# insert sizes
+# insert size
 # --------------------------------------------------------
-insert_size_150=150
+insert_size=300
 
 # output directories for targets
 # --------------------------------------------------------
-dirRefs_2x150=$dirWrk/target_info/2x150
+genomeTargetInfo=$wd/input/genome_target_info
 
 # number of threads for pipeline and clustering
 # --------------------------------------------------------
-threadsPipeline=10
+threadsPipeline=4
 threadsClustering=2
 
-# ********************************************************
-# SeekDeep - get target info from genomes
-# ********************************************************
-
-# # generate the target info
-# # --------------------------------------------------------
-# SeekDeep genTargetInfoFromGenomes \
-#    --primers $primers_2x150 \
-#    --pairedEndLength $insert_size_150 \
-#    --genomeDir $resources/genomes/ \
-#    --gffDir $resources/info/gff \
-#    --errors 2 \
-#    --dout $dirRefs_2x150 \
-#    --numThreads $threadsPipeline \
-#    --shortNames \
-#    --overWriteDir
-
-# ********************************************************
-# SeekDeep - setup tar amp analysis
-# ********************************************************
-
-# generate wrapper scripts
+# define a variable for control samples to be excluded from population analysis
+# this is applicable if you have control samples in your dataset, which this test
+# dataset does not have
 # --------------------------------------------------------
+control_samples="Control-3D7,Control-7G8,Control-HB3"
+
+# ========================================================
+# SeekDeep - setup tar amp analysis to generate wrapper scripts
+# ========================================================
+
 SeekDeep setupTarAmpAnalysis \
-   --samples $sampleNames_2x150 \
-   --outDir $dirOut_2x150 \
+   --samples $sampleNames \
+   --outDir $out_dir \
    --technology nanopore \
-   --uniqueKmersPerTarget $dirRefs_2x150/forSeekDeep/uniqueKmers.tab.txt.gz \
-   --inputDir $dirData \
-   --idFile $primers_2x150 \
-   --lenCutOffs $dirRefs_2x150/forSeekDeep/lenCutOffs.txt \
+   --uniqueKmersPerTarget $genomeTargetInfo/forSeekDeep/uniqueKmers.tab.txt.gz \
+   --inputDir $data_dir \
+   --idFile $primers \
+   --lenCutOffs $genomeTargetInfo/forSeekDeep/lenCutOffs.txt \
    --doNotGuessRecFlags \
    --extraExtractorCmds="--primerWithinStart 100 \
                          --minLenCutOff 100 \
                          --qualCheckLevel 11" \
    --extraKlusterCmds="--readLengthMinDiff 100 \
                        --numThreads $threadsClustering" \
-   --extraProcessClusterCmds="--controlSamples Control-3D7,Control-7G8,Control-HB3 \
+   --extraProcessClusterCmds="--controlSamples $control_samples \
                               --sampleMinTotalReadCutOff 250 \
                               --replicateMinTotalReadCutOff 250 \
                               --lowFreqHaplotypeFracCutOff 0.05 \
-                              --gffFnp $resources/info/gff/Pf3D7.gff \
-                              --genomeFnp $resources/genomes/Pf3D7.fasta \
-                              --knownAminoAcidChangesFnp $resources/info/pf_drug_resistant_aaPositions_k13_all.tsv" \
-   --previousPopSeqsDir $dirRefs_2x150/forSeekDeep/refSeqs \
+                              --gffFnp $reference/info/gff/Pf3D7.gff \
+                              --genomeFnp $reference/genomes/Pf3D7.fasta \
+                              --knownAminoAcidChangesFnp $reference/info/pf_drug_resistant_aaPositions.tsv" \
+   --previousPopSeqsDir $genomeTargetInfo/forSeekDeep/refSeqs \
    --numThreads $threadsPipeline
 
 # run the analysis
 # --------------------------------------------------------
-cd $dirOut_2x150
+cd $out_dir
 ./runAnalysis.sh $threadsPipeline
 
-# ********************************************************
+# ========================================================
 # unload modules
-# ********************************************************
+# ========================================================
 
-# module unload gcc/10.2
-# module unload samtools/1.16.1
-# module unload bowtie2/2.4.2
+# module unload gcc
+# module unload samtools
+# module unload bowtie2
 
-# ********************************************************
+# ========================================================
 # deactivate conda environment
-# ********************************************************
+# ========================================================
 
 conda deactivate
