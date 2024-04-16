3f873fa84a950451f98a47821c94262fb1a90bd1
Kevin Wamae <wamaekevin@gmail.com>
2024-04-16 16:39:52 +0300

    refactor to use multiprocessing for speed

--- wf-demux-nanopore-mids/scripts/fastq-demux-mid.py
+++ wf-demux-nanopore-mids/scripts/fastq-demux-mid.py
@@ -1,6 +1,7 @@
 import gzip
 from Bio import SeqIO
 import os
+from multiprocessing import Pool
 
 
 def read_mids(mid_file):
@@ -25,29 +26,32 @@ def read_primers(primer_file):
     return primers
 
 
-def demultiplex_fastq(input_dir, output_dir, mids, primers):
-    # Loop over all barcode files
-    for i in range(1, 97):
-        input_file = f"{input_dir}/barcode{str(i).zfill(2)}.fastq.gz"
-        if not os.path.exists(input_file):
-            continue
-        with gzip.open(input_file, "rt") as handle:
-            for record in SeqIO.parse(handle, "fastq"):
-                sequence = str(record.seq)
-                # Check each primer in the sequence
-                for primer_seq, target in primers.items():
-                    primer_pos = sequence.find(primer_seq)
-                    if primer_pos != -1 and primer_pos >= 10:
-                        mid_seq = sequence[primer_pos - 10 : primer_pos]
-                        if mid_seq in mids:
-                            mid_name = mids[mid_seq]
-                            barcode = input_file.split("/")[-1].split(".")[0]
-                            output_file = (
-                                f"{output_dir}/{barcode}-{target}-{mid_name}.fastq.gz"
-                            )
-                            # Open the output file in append mode with gzipped format
-                            with gzip.open(output_file, "at") as output_handle:
-                                SeqIO.write(record, output_handle, "fastq")
+def process_file(input_file):
+    with gzip.open(input_file, "rt") as handle:
+        for record in SeqIO.parse(handle, "fastq"):
+            sequence = str(record.seq)
+            for primer_seq, target in primers.items():
+                primer_pos = sequence.find(primer_seq)
+                if primer_pos != -1 and primer_pos >= 10:
+                    mid_seq = sequence[primer_pos - 10 : primer_pos]
+                    if mid_seq in mids:
+                        mid_name = mids[mid_seq]
+                        barcode = input_file.split("/")[-1].split(".")[0]
+                        output_file = (
+                            f"{output_dir}/{barcode}-{target}-{mid_name}.fastq.gz"
+                        )
+                        with gzip.open(output_file, "at") as output_handle:
+                            SeqIO.write(record, output_handle, "fastq")
+
+
+def demultiplex_fastq(input_dir, output_dir):
+    input_files = [
+        f"{input_dir}/barcode{str(i).zfill(2)}.fastq.gz"
+        for i in range(1, 97)
+        if os.path.exists(f"{input_dir}/barcode{str(i).zfill(2)}.fastq.gz")
+    ]
+    with Pool() as pool:
+        pool.map(process_file, input_files)
 
 
 # Read MID and primer information
@@ -55,11 +59,11 @@ mids = read_mids("input/run_files/mids.tsv")
 primers = read_primers("input/run_files/primers.tsv")
 
 # Directory containing barcode FASTQ files
-input_dir = "input/fastq"
+input_dir = "/data/isabella_group/data/turkana_embatalk/2024_04_12_nanopore_r10.4.1/input/fastq_barcodes_merged"
 # Directory to save demultiplexed FASTQ files
-output_dir = "output"
+output_dir = "/data/isabella_group/data/turkana_embatalk/2024_04_12_nanopore_r10.4.1/input/fastq_demultiplexed"
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 # Process the files
-demultiplex_fastq(input_dir, output_dir, mids, primers)
+demultiplex_fastq(input_dir, output_dir)
